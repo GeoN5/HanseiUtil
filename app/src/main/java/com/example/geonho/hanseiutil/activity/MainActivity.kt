5f1fc9fc1baacb222530e8ea39f598c15412f66d
GeoN5 <sgepyh2916@naver.com>
2018-11-26 18:56:11 +0900

    BottomBar implementation & Setting

---
+++ app/src/main/java/com/example/geonho/hanseiutil/activity/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.geonho.hanseiutil.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.widget.Toast
+import com.example.geonho.hanseiutil.R
+import com.example.geonho.hanseiutil.fragment.MealFragment
+import com.example.geonho.hanseiutil.fragment.SettingFragment
+import com.example.geonho.hanseiutil.fragment.TimeTableFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val FINSH_INTERVAL_TIME = 2000
+    private var backPressedTime:Long = 0
+    lateinit var fragment : Fragment
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_meal -> {
+                    fragment = MealFragment.newInstance()
+                }
+                R.id.action_timeTable -> {
+                    fragment = TimeTableFragment.newInstance()
+                }
+                R.id.action_setting -> {
+                    fragment = SettingFragment.newInstance()
+                }
+
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
+            true
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init(){
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, MealFragment.newInstance()).commit()
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - backPressedTime
+        if (intervalTime in 0..FINSH_INTERVAL_TIME) {
+            ActivityCompat.finishAffinity(this)
+        } else {
+            backPressedTime = tempTime
+            Toast.makeText(applicationContext, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/geonho/hanseiutil/activity/SplashActivity.kt
+++ app/src/main/java/com/example/geonho/hanseiutil/activity/SplashActivity.kt
@@ -1,12 +1,12 @@
-package com.example.geonho.hanseiutil
+package com.example.geonho.hanseiutil.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
